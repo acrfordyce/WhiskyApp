bd078c1a0e038fcce25d715cbcf8c0cd715a9d00
acrfordyce <alexandercrfordyce@gmail.com>
2015-02-11 21:59:07 -0800

    update db schema

--- app/models.py
+++ app/models.py
@@ -7,3 +7,15 @@ class Whisky(db.Model):
     age_statement = db.Column(db.String(5))
     region = db.Column(db.String(120))
     abv = db.Column(db.Float)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nickname = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    reviews = db.relationship('Review', backref='author', lazy='dynamic')
+
+class Review(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(500))
+    timestamp= db.Column(db.DateTime)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
