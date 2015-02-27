bf35e710cf93e1014ab81d03b5b6fc701bf8ccb9
acrfordyce <alexandercrfordyce@gmail.com>
2015-02-26 21:24:22 -0800

    Add local datetime display functionality

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
+from .momentjs import Momentjs
 
 
 app = Flask(__name__)
 app.config.from_object('config')
+app.jinja_env.globals['momentjs'] = Momentjs
 db = SQLAlchemy(app)
 lm = LoginManager()
 lm.init_app(app)
